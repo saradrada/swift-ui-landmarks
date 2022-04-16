d1c4a2a651a2e1d2ec2e1263dc1bfd9445ec0dcb
saradrada <saraodrada@gmail.com>
2022-04-16 13:15:38 +0200

    Add Map view from MapKit to render landmark location

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Sara Ortiz on 16.04.22.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+
+  @State private var region = MKCoordinateRegion(
+  center: CLLocationCoordinate2D(latitude: 52.5162746, longitude: 13.3777041),
+  span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+
+  var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
