64e7261e4ba0ea6e3e18380d013fc7b27cb49869
saradrada <saraodrada@gmail.com>
2022-04-19 14:51:37 +0200

    Add landmarks images

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Sara Ortiz on 17.04.22.
+//
+
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+
+  var id: Int
+  var name: String
+  var park: String
+  var state: String
+  var description: String`  ASw3
+}
